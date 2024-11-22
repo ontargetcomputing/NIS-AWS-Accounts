b2d8640d35198ddb26b9725c6c7694768c0df24f
balutbomber <balutbomber@gmail.com>
2024-11-21 17:11:31 -0800

    fixed terraform

--- accounts/SBOX-9394/baseline/baseline-workspace/s3_bucket_IMPORT1.tf
+++ accounts/SBOX-9394/baseline/baseline-workspace/s3_bucket_IMPORT1.tf
@@ -1,25 +1,11 @@
+data "aws_canonical_user_id" "current" {}
+
 resource "aws_s3_bucket" "cargotank" {
   bucket        = "cargotank"
   force_destroy = "false"
 
-  grant {
-    id          = "b3b23dd60e4ea2a601e68fae3b61071102e13dc3ed0c1d6455fe0b715239a9e5"
-    permissions = ["FULL_CONTROL"]
-    type        = "CanonicalUser"
-  }
 
   object_lock_enabled = "false"
-  request_payer       = "BucketOwner"
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-
-      bucket_key_enabled = "true"
-    }
-  }
 
   tags = {
     IMPORT1 = "true"
@@ -30,9 +16,46 @@ resource "aws_s3_bucket" "cargotank" {
     IMPORT1 = "true"
     Name    = "cargotank"
   }
+}
 
-  versioning {
-    enabled    = "false"
-    mfa_delete = "false"
+resource "aws_s3_bucket_versioning" "cargotank_versioning" {
+  bucket = aws_s3_bucket.cargotank.id
+  versioning_configuration {
+    status = "Enabled"
+    mfa_delete = "Disabled"
   }
+}
+
+resource "aws_s3_bucket_acl" "cargotank_bucket_acl" {
+  bucket = aws_s3_bucket.cargotank.id
+  access_control_policy {
+    grant {
+      grantee {
+        id   = "b3b23dd60e4ea2a601e68fae3b61071102e13dc3ed0c1d6455fe0b715239a9e5"
+        type = "CanonicalUser"
+      }
+      permission = "FULL_CONTROL"
+    }
+
+    owner {
+      id = data.aws_canonical_user_id.current.id
+    }
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "cargotank_server_side_encryption_configuration" {
+  bucket = aws_s3_bucket.cargotank.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+
+    bucket_key_enabled = "true"
+  }
+}
+
+resource "aws_s3_bucket_request_payment_configuration" "cargotank_request_payment_configuration" {
+  bucket = aws_s3_bucket.cargotank.bucket
+  payer  = "BucketOwner"
 }
