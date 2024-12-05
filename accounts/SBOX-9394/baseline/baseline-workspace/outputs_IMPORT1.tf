50a19df3f218f47e38357b96bdcf6a27f5256734
balutbomber <balutbomber@gmail.com>
2024-12-05 10:29:40 -0800

    latest

--- accounts/SBOX-9394/baseline/baseline-workspace/outputs_IMPORT1.tf
+++
@@ -1,7 +0,0 @@
-output "aws_iam_policy_adarsh_id" {
-  value = "${aws_iam_policy.adarsh.id}"
-}
-
-output "aws_iam_role_balutbomber-terraform-admin-role_id" {
-  value = "${aws_iam_role.balutbomber-terraform-admin-role.id}"
-}
